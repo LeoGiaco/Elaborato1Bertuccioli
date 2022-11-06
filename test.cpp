5460fc9d2df72b498772976ecf7037dab55b32dd
LeoGiaco <leogiaco2@gmail.com>
2022-11-07 00:19:04 +0100

    Refactoring, add movement

--- test.cpp
+++ test.cpp
@@ -18,10 +18,23 @@ Window w(&program, &scene, WINDOW_WIDTH, WINDOW_HEIGHT, ortho(0.0f, (float)WINDO
 
 mat4 Projection;
 
-Shape *s1, *s2, *s3, *s4, *s5, *sea;
-ComplexShape *cs;
-
+Shape *sea;
+ComplexShape *boat;
+Shape *enemy1, *enemy2, *enemy3, *activeEnemy;
 float t = 0;
+
+float reload = 0;
+int moving = 0, dir = 1;
+
+Shape *chooseRandomEnemy()
+{
+    return enemy1; // TODO: implement.
+}
+
+void fire()
+{
+    reload = 2;
+}
 
 void drawCallback()
 {
@@ -30,11 +43,15 @@ void drawCallback()
 
 void updateCallback(int v)
 {
+    if (reload > 0)
+        reload -= DELTA_T;
+
     t += DELTA_T;
     if (t > 360)
         t -= 360;
 
-    cs->setRotationAroundAnchor(radians(10 * sin(2 * t)));
+    boat->setRotationAroundAnchor(radians(10 * sin(2 * t)));
+    boat->shiftAnchorX(moving * 80 * DELTA_T);
 
     glutTimerFunc(w.getUpdateDelay(), updateCallback, 0);
     glutPostRedisplay();
@@ -45,7 +62,51 @@ void reshapeCallback(int width, int height)
     w.reshape(width, height);
 }
 
-void createShapes()
+void keyboardCallback(unsigned char key, int mouseX, int mouseY)
+{
+    if (key == ' ')
+    {
+        fire();
+    }
+}
+
+void keyboardCallbackSp(int key, int mouseX, int mouseY)
+{
+    switch (key)
+    {
+    case GLUT_KEY_LEFT:
+        moving = -1;
+        dir = -1;
+        boat->setScaleRelativeToAnchor(-1, 1);
+        break;
+    case GLUT_KEY_RIGHT:
+        moving = 1;
+        dir = 1;
+        boat->setScaleRelativeToAnchor(1, 1);
+        break;
+    default:
+        break;
+    }
+}
+
+void keyboardCallbackUpSp(int key, int mouseX, int mouseY)
+{
+    switch (key)
+    {
+    case GLUT_KEY_LEFT:
+        if (moving < 0) // Proofed against pressing both keys and releasing the first.
+            moving = 0;
+        break;
+    case GLUT_KEY_RIGHT:
+        if (moving > 0)
+            moving = 0;
+        break;
+    default:
+        break;
+    }
+}
+
+void createBoat(ComplexShape **boat)
 {
     ////////// boat
     vector<vec3> samples;
@@ -69,7 +130,7 @@ void createShapes()
     derivs.push_back(vec2(0.365f, 0.0273f));
     derivs.push_back(vec2(0.7267f, 0.3667f));
 
-    s1 = Shape::HermiteCurve(&program, 4, samples, derivs, vec4(1, 0, 0, 1), true, vec3(0, 0.2f, 0), vec4(1, 1, 0, 1));
+    Shape *s1 = Shape::HermiteCurve(&program, 4, samples, derivs, vec4(1, 0, 0, 1), true, vec3(0, 0.2f, 0), vec4(1, 1, 0, 1));
     s1->setScale(100);
     //////////
     ////////// mainmast
@@ -88,7 +149,7 @@ void createShapes()
         colorsRect.push_back(vec4(0.7f, 0.7f, 0.7f, 1));
     }
 
-    s2 = new Shape(&program, verticesRect, colorsRect, GL_TRIANGLE_FAN);
+    Shape *s2 = new Shape(&program, verticesRect, colorsRect, GL_TRIANGLE_FAN);
     s2->setScale(80);
     s2->setPosition(0, 110);
     //////////
@@ -114,7 +175,7 @@ void createShapes()
     derivs.push_back(vec2(0.6037f, -0.002f));
     derivs.push_back(vec2(0.7407f, 0.6059f));
 
-    s3 = Shape::HermiteCurve(&program, 4, samples, derivs, vec4(1, 1, 1, 1), true, vec3(0.6f, 0, 0), vec4(0.8, 0.8, 0.8, 1));
+    Shape *s3 = Shape::HermiteCurve(&program, 4, samples, derivs, vec4(1, 1, 1, 1), true, vec3(0.6f, 0, 0), vec4(0.8, 0.8, 0.8, 1));
     s3->setScale(70);
     s3->setPosition(0, 130);
     //////////
@@ -134,30 +195,44 @@ void createShapes()
         colorsRect.push_back(vec4(0.3f, 0.3f, 0.3f, 1));
     }
 
-    s4 = new Shape(&program, verticesRect, colorsRect, GL_TRIANGLE_FAN);
+    Shape *s4 = new Shape(&program, verticesRect, colorsRect, GL_TRIANGLE_FAN);
     s4->setScale(50);
     s4->setPosition(40, 40);
     //////////
     ////////// hole
-    s5 = Shape::circle(&program, 12, vec4(0, 0, 0, 1), vec4(0, 0, 0, 1));
+    Shape *s5 = Shape::circle(&program, 12, vec4(0, 0, 0, 1), vec4(0, 0, 0, 1));
     s5->setScale(10);
     s5->setPosition(40, 40);
     //////////
 
-    sea = Shape::circle(&program, 4, vec4(0, 0.3f, 0.75f, 0.8f), vec4(0, 0.3f, 0.75f, 0.8f)); // Will become a curve.
-    sea->rotate(radians(45.0f));
-    sea->setPosition(w.getWidth() / 2.0f, 0);
-    sea->setScale(w.getWidth() / 2.0f * 1.45f);
+    (*boat) = new ComplexShape(&program);
+    (*boat)->addShape(s2);
+    (*boat)->addShape(s3);
+    (*boat)->addShape(s1);
+    (*boat)->addShape(s5);
+    (*boat)->addShape(s4);
+    (*boat)->setAnchorPosition(WINDOW_WIDTH / 2.0f, WINDOW_HEIGHT / 2.0f);
+}
 
-    cs = new ComplexShape(&program);
-    cs->addShape(s2);
-    cs->addShape(s3);
-    cs->addShape(s1);
-    cs->addShape(s5);
-    cs->addShape(s4);
-    cs->setAnchorPosition(WINDOW_WIDTH / 2.0f, WINDOW_HEIGHT / 2.0f);
+void createEnemies(Shape **e1, Shape **e2, Shape **e3)
+{
+}
 
-    scene.addShape(cs);
+void createSea(Shape **sea)
+{
+    (*sea) = Shape::circle(&program, 4, vec4(0, 0.3f, 0.75f, 0.8f), vec4(0, 0.3f, 0.75f, 0.8f)); // Will become a curve.
+    (*sea)->rotate(radians(45.0f));
+    (*sea)->setPosition(w.getWidth() / 2.0f, 0);
+    (*sea)->setScale(w.getWidth() / 2.0f * 1.45f);
+}
+
+void createShapes()
+{
+    createBoat(&boat);
+    createEnemies(&enemy1, &enemy2, &enemy3);
+    createSea(&sea);
+
+    scene.addShape(boat);
     scene.addShape(sea);
 }
 
@@ -171,6 +246,11 @@ int main(int argc, char *argv[])
     glClearColor(0, 0, 0.2f, 1.0f);
 
     createShapes();
+
+    glutKeyboardFunc(keyboardCallback);
+    glutSpecialFunc(keyboardCallbackSp);
+    glutSpecialUpFunc(keyboardCallbackUpSp);
+    glutIgnoreKeyRepeat(1);
 
     w.loop();
 }
