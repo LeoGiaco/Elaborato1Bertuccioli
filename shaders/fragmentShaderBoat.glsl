860835d09ed1f597c273af24318081c491eea877
LeoGiaco <leogiaco2@gmail.com>
2022-11-11 23:31:47 +0100

    Create shaders for boat

--- shaders/fragmentShaderBoat.glsl
+++ shaders/fragmentShaderBoat.glsl
@@ -1,31 +1,27 @@
+#version 330 core
 
-uniform float time;	// remove
 uniform vec2 resolution;
+uniform int stripes; // 20
+uniform float stripe_size; // 0.2
+uniform float angle;
+uniform vec2 off;
 
-int stripes = 20;	// stripes as uniform
-float stripe_size = 0.2;
-float angle = time / 2.0;	// angle as uniform
-vec2 offset = vec2(0.5, 0.5);	// offset as uniform
+uniform vec4 base_color;
+uniform vec4 stripe_color;
 
 #define BETWEEN(a, x, b) ((a) < (x) && (x) < (b))
 
 float ratio = resolution.x / resolution.y;
 
 void main( void ) {
-
-	vec2 position = ( gl_FragCoord.xy / resolution.xy - offset );
-	vec4 color = vec4(0.35, 0.0, 0.0, 1.0);
-
+	vec2 position = (gl_FragCoord.xy / resolution.xy - off);
+	vec4 color = stripe_color;
 
 	// float modv = mod((position.x + 1.0 / 2.0) * cos(angle) + (position.y + 1.0) * sin(angle), 1.0 / float(stripes));
 	float modv = mod(position.x * cos(angle) * ratio + position.y * sin(angle), 1.0 / float(stripes));
 
 	if  ( BETWEEN((stripe_size / 2.0) / float(stripes), modv, (1.0 - stripe_size / 2.0) / float(stripes))) {
-		color = vec4(0.4, 0.15, 0.0, 1.0);
-	}
-
-	if ( distance(vec2(position.x * ratio, position.y), vec2(0.0, 0.0)) < 0.02 ) {	// shows rotation center
-		color = vec4(1.0, 1.0, 1.0, 1.0);
+		color = base_color;
 	}
 
 	gl_FragColor = color;
